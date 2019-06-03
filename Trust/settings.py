d2747879b09b88c1a09b31b9489d06486c2ebe8a
Kishan51 <burrikishan@gmail.com>
2019-06-03 16:32:25 +0530

    Css change

--- Trust/settings.py
+++ Trust/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'fs+!(z9g@zo608nb%__kl5*qk=4227mcl#b1$6^k-8or%-7h65'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['uqaab-trust.herokuapp.com']
+ALLOWED_HOSTS = ['uqaab-trust.herokuapp.com','127.0.0.1']
 ########## EMAIL CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
 ########## END EMAIL CONFIGURATION
